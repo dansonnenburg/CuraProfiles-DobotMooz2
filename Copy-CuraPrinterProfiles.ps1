fa29ad4b47f270886e5f4459a98837e291d3b092
Dan Sonnenburg <dan.sonnenburg@gmail.com>
2018-07-20 15:47:21 -0500

    fix: removed extruder profile

--- Copy-CuraPrinterProfiles.ps1
+++ Copy-CuraPrinterProfiles.ps1
@@ -1,10 +1,8 @@
 ﻿#Requires -RunAsAdministrator
+#Purpose: Copies Dobot Mooz 1 & 2 3D printer profile to Cura definitions
+#Author: Dan Sonnenburg
+#Created: 7/20/2018
 
 $PrinterDefinition = Join-Path $PSScriptRoot DobotMooz_1-2.def.json
-$ExtruderDefinition = Join-Path $PSScriptRoot Mooz_1-2_extruder_1.def.json
-
 $DefinitionDir = 'C:\Program Files\Ultimaker Cura 3.4\resources\definitions'
-$ExtruderDir = 'C:\Program Files\Ultimaker Cura 3.4\resources\extruders'
-
 Copy-Item "$PrinterDefinition" "$DefinitionDir"
-#Copy-Item "$ExtruderDefinition" "$ExtruderDir"
