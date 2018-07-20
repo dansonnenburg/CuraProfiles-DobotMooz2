9c47af026e5dd1fd5693c81a996669009132c4b2
Dan Sonnenburg <dan.sonnenburg@gmail.com>
2018-07-20 15:44:56 -0500

    chore: initial commit

---
+++ Copy-CuraPrinterProfiles.ps1
@@ -0,0 +1,10 @@
+﻿#Requires -RunAsAdministrator
+
+$PrinterDefinition = Join-Path $PSScriptRoot DobotMooz_1-2.def.json
+$ExtruderDefinition = Join-Path $PSScriptRoot Mooz_1-2_extruder_1.def.json
+
+$DefinitionDir = 'C:\Program Files\Ultimaker Cura 3.4\resources\definitions'
+$ExtruderDir = 'C:\Program Files\Ultimaker Cura 3.4\resources\extruders'
+
+Copy-Item "$PrinterDefinition" "$DefinitionDir"
+#Copy-Item "$ExtruderDefinition" "$ExtruderDir"
